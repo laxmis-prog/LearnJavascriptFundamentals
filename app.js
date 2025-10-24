d68a7d13123ddf9336f9ffac04b63aa1662ad7d2
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-24 10:17:03 +0300

    In the second console.log() statement use .trim() to remove spaces before and after the words 'Remove whitespace'

--- app.js
+++ app.js
@@ -2,4 +2,4 @@
 console.log('Codecademy'.toUpperCase());
 
 // Use a string method to log the following string without whitespace at the beginning and end of it
-console.log('    Remove whitespace   '.toUpperCase());
+console.log('    Remove whitespace   '.trim());
