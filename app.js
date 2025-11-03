b5505b055c2e72b3393c9eae796ef3c1c7d2ab43
laxmis-prog <laxmisanuu5@gmail.com>
2025-11-03 10:33:19 +0200

    Use the /= mathematical assignment operator to divide the value stored in quarterMe by 4.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let quarterMe = 1152;
 levelUp += 5;
 powerLevel -= 100;
 multiplyMe *= 11;
+quarterMe /= 4;
 
 // These console.log() statements below will help you check the values of the variables.
 // You do not need to edit these statements.
